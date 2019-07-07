66c500318d4793a0b6a39cde904f339c97be7510
stu-29 <768962537@qq.com>
2019-07-07 17:05:01 +0800

    我知道了。

--- class02/ViewController.swift
+++ class02/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var result :float_t = 10.2
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        let 如果我去超市=true
+        
+        let 如果看到橘子 = true
+        
+        if 如果我去超市{
+            var 买西瓜的数量=1
+            
+            if 如果看到橘子{
+                买西瓜的数量=10
+            }
+            print("我要买：\(买西瓜的数量)颗西瓜")
+        }
+        
+        
+//        if (如果我去超市 ){
+//
+//            var number = 1
+//
+//
+//        else if(如果看到橘子){
+//
+//             var number = 10
+//
+//         }
+//          print("买\(number)个西瓜")
+//    }
+    func add(a :Int,b:Int) -> Int  {
+        return a + b
     }
-
 
 }
 
+}
